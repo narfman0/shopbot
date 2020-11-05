ffe686f827bdc27500e4efe0114187c20f9fcf32
Jon Robison <narfman0@gmail.com>
2020-11-05 08:58:04 -0500

    -Fix tragic bool parsing bug :)

--- shopbot/settings.py
+++ shopbot/settings.py
@@ -1,7 +1,7 @@
 import os
 
 
-BREAKPOINT_ON_SUCCESS = bool(os.environ.get("BREAKPOINT_ON_SUCCESS", "False"))
-BUY_NOW = bool(os.environ.get("BUY_NOW", "False"))
-ABORT_ON_FAILURE = bool(os.environ.get("ABORT_ON_FAILURE", "False"))
+BREAKPOINT_ON_SUCCESS = bool(os.environ.get("BREAKPOINT_ON_SUCCESS", "True"))
+BUY_NOW = bool(os.environ.get("BUY_NOW", ""))
+ABORT_ON_FAILURE = bool(os.environ.get("ABORT_ON_FAILURE", ""))
 CVV2 = os.environ["CVV2"]
